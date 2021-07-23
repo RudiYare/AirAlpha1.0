e4b62a7c08136bce7523afb4137bcaea48b79df1
Ярослав-ПК <>
2021-07-23 17:24:24 +0300

    FX 1,5 day

--- src/YAROSLAV/Main.java
+++ src/YAROSLAV/Main.java
@@ -3,7 +3,10 @@ package YAROSLAV;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
 import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
@@ -20,17 +23,24 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("AIR ALFA 1.0");
-        primaryStage.setResizable(false);
         Group group = new Group();
-        primaryStage.setFullScreen(true);
-        Scene scene = new Scene(group, 1024 , 720);
-        //primaryStage.setOpacity(25);
-        Text text = new Text  (100, 100, "ПОЕХАЛИ");
-        text.setFont(Font.font(50));
-        //text.setFill(Color.BLANCHEDALMOND);
-        group.getChildren().add(text);
+        primaryStage.setResizable(false);
+        Scene scene = new Scene(group, 500,500);
+        Canvas cnvs = new Canvas(100,100);
+        GraphicsContext gc = cnvs.getGraphicsContext2D();
+
+        drawShapess(gc);
+
+        group.getChildren().add(cnvs);
+
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+    private void drawShapess(GraphicsContext gc){
+        gc.setFill(Color.GREENYELLOW);
+        gc.setStroke(Color.AZURE);
+        gc.fillArc(50,50,50,25,90,100, ArcType.ROUND);
+        gc.fillRect(0,0,50,35);
+    }
+
 }

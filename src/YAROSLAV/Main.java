f40f0ec01b6f0044db48a036353d771559e97c7c
Ярослав-ПК <>
2021-07-24 23:01:59 +0300

    FX 1,5 day

--- src/YAROSLAV/Main.java
+++ src/YAROSLAV/Main.java
@@ -1,65 +1,58 @@
+
+
 package YAROSLAV;
 
 import javafx.application.Application;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.ArcType;
-import javafx.scene.shape.Ellipse;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
+    public Main() {
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    @Override
     public void start(Stage primaryStage) {
-        Group group = new Group();
         primaryStage.setResizable(false);
-        Scene scene = new Scene(group, 500,500);
-        Text text = new Text(250,250,"В чем СИЛА, брат?");
-        group.getChildren().addAll(text);
-        BorderPane border = new BorderPane();
-        border.setMinWidth(250);
-        border.setMinHeight(250);
-        Button newBtn = new Button("В деньгах");
-        border.setLeft(newBtn);
-        border.setBottom(newBtn);
-        border.getLeft();
-        group.getChildren().addAll(border);
-
-        BorderPane border1 = new BorderPane();
-        border1.setMinHeight(250);
-        border1.setMinWidth(250);
-
-        border.setRight(new Button("В правде"));
-        group.getChildren().addAll(border1);
-
-        primaryStage.setScene(scene);
+        Group group = new Group();
+        Scene scene1 = new Scene(group, 500.0D, 500.0D);
+        Button but1 = new Button("Rammstein");
+        but1.setLayoutX(0.0D);
+        but1.setLayoutY(0.0D);
+        Button but2 = new Button("Rammstain");
+        but2.setLayoutX(50.0D);
+        but2.setLayoutY(50.0D);
+        group.getChildren().addAll(new Node[]{but1, but2});
+        but1.setOnAction((actionEvent) -> {
+            Text text = new Text(0.0D, 20.0D, "EEEEE");
+            Scene scene2 = new Scene(new Group(new Node[]{text}), 200.0D, 200.0D);
+            Stage stage2 = new Stage();
+            stage2.setScene(scene2);
+            stage2.setResizable(false);
+            stage2.initModality(Modality.WINDOW_MODAL);
+            stage2.initOwner(primaryStage);
+            stage2.show();
+        });
+        but2.setOnAction((actionEvent) -> {
+            Text text = new Text(0.0D, 20.0D, "ДЕБИЛ, В АД НАУЙ!");
+            Scene scene2 = new Scene(new Group(new Node[]{text}), 200.0D, 200.0D);
+            Stage stage2 = new Stage();
+            stage2.setScene(scene2);
+            stage2.setResizable(false);
+            stage2.initModality(Modality.WINDOW_MODAL);
+            stage2.initOwner(primaryStage);
+            stage2.show();
+        });
+        primaryStage.setScene(scene1);
         primaryStage.show();
     }
-    private void drawShapess(GraphicsContext gc){
-
-
-    }
-    private void drawShapess1(GraphicsContext gc){
-        gc.setFill(Color.GREENYELLOW);
-        gc.setStroke(Color.BLACK);
-        gc.fillOval(12.5,12.5,25,25);
-        gc.strokeOval(0,0,50,50);
-        //gc.fillArc(50,50,50,25,90,100, ArcType.ROUND);
-        //gc.fillRect(0,0,50,35);
-    }
-
 }

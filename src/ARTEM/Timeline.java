49309eb3eb22dcb16c197559173a5675e690783e
Joggor <artem_zinatulin643@mail.ru>
2021-08-12 21:19:25 +0300

    Commit

--- src/ARTEM/Timeline.java
+++ src/ARTEM/Timeline.java
@@ -2,15 +2,13 @@ package ARTEM;
 
 public class Timeline {
 
-    public int ID;
-    private Airport starting_airport;
-    private Airport finishing_airport;
+    private int starting_airport;
+    private int finishing_airport;
     private int starting_time;
     private int flight_time;
     private int price;
 
-    public Timeline(int ID, Airport starting_airport, Airport finishing_airport, int starting_time, int flight_time, int price) {
-        this.ID = ID;
+    public Timeline(int starting_airport, int finishing_airport, int starting_time, int flight_time, int price) {
         this.starting_airport = starting_airport;
         this.finishing_airport = finishing_airport;
         this.starting_time = starting_time;

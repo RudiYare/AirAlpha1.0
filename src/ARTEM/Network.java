2f5aa435ea0b2cad47cc8cabd23677c854909708
Joggor <artem_zinatulin643@mail.ru>
2021-10-08 23:08:13 +0300

    FX$

--- src/ARTEM/Network.java
+++ src/ARTEM/Network.java
@@ -17,30 +17,6 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
-class Tree {
-    public ArrayList<Tree> children;
-    public Tree parent;
-    public String title;
-    public Double x;
-    public Double y;
-    public int ID;
-
-    public Tree(Tree parent, String title) {
-        this.parent = parent;
-        this.title = title;
-        this.children = new ArrayList();
-        this.x = null;
-        this.y = null;
-    }
-
-    public Tree(Tree parent, String title, double x, double y) {
-        this.parent = parent;
-        this.title = title;
-        this.children = new ArrayList();
-        this.x = x;
-        this.y = y;
-    }
-}
 
 
 public class Network {

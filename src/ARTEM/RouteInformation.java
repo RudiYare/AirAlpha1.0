58410ee0ce0b1d90249dd5e9443b38910abe2c7d
Joggor <artem_zinatulin643@mail.ru>
2021-10-08 22:06:43 +0300

    FX4

--- src/ARTEM/RouteInformation.java
+++ src/ARTEM/RouteInformation.java
@@ -1,6 +1,8 @@
 package ARTEM;
 
 
+import javafx.util.Pair;
+
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Map;
@@ -11,13 +13,17 @@ public class RouteInformation {
     public long starting_time;
     private final long SECONDS_IN_WEEK = 604800000;
     public ArrayList<Timeline> transfers;
+    public ArrayList<Pair<Long, Long>> transfer_time;
     private ArrayList<String> result;
     public RouteInformation(ArrayList<Timeline> transfers,
                             int starting_airport,
-                            int finishing_airport, long starting_time) {
+                            int finishing_airport,
+                            ArrayList<Pair<Long, Long>> transfer_time,
+                            long starting_time) {
         this.transfers = transfers;
         this.starting_airport = starting_airport;
         this.finishing_airport = finishing_airport;
+        this.transfer_time = transfer_time;
         this.starting_time = starting_time;
     }
 
@@ -46,43 +52,31 @@ public class RouteInformation {
         }
         result.add(day + "." + month + "." + year + " " + hours + ":" + minute);
     }
-
     public ArrayList<String> init(Network net) {
         result = new ArrayList<>();
-        System.out.println("");
         result.add(net.getAirportInformationAsString(starting_airport));
         result.add(net.getAirportInformationAsString(finishing_airport));
-        long flight_time = 0;
-        long current_time = starting_time;
-        for (var i = 0; i < transfers.size(); i++) {
-            //transfers.get(i).print();
-            long starts_in = current_time % SECONDS_IN_WEEK;
-            long flight_start = transfers.get(i).getStartingTime();
-            System.out.print(starts_in); System.out.print(" "); System.out.println(flight_start);
-            if (flight_start < starts_in) {
-                flight_start += SECONDS_IN_WEEK;
-            }
-            flight_time += flight_start - starts_in + transfers.get(i).getFlightTime();
-            current_time += flight_start - starts_in + transfers.get(i).getFlightTime();
-        }
+        long flight_time = transfer_time.get(0).getValue() - starting_time;
         result.add(Long.toString(flight_time / 1000 / 3600) + " ч. " + Long.toString((flight_time / 60000) % 60) + " мин.");
         double flight_price = 0;
-        for (int transfer = 0; transfer < transfers.size(); transfer++) {
-            flight_price += transfers.get(transfer).getPrice();
+        for (Timeline timeline : transfers) {
+            flight_price += timeline.getPrice();
         }
         result.add(Double.toString(Math.round(flight_price * 100) / 100.0));
         Timestamp date;
-        date = new Timestamp(starting_time);
+
+        date = new Timestamp(transfer_time.get(transfer_time.size() - 1).getKey());
         addNewDateToRoute(date);
-        date = new Timestamp(starting_time + flight_time);
+        date = new Timestamp(transfer_time.get(0).getValue());
         addNewDateToRoute(date);
         result.add(Integer.toString(transfers.size() - 1));
+
         for (int transfer = 0; transfer < transfers.size(); transfer++) {
             result.add(net.getAirportInformationAsString(transfers.get(transfer).getStartingAirport()));
             result.add(net.getAirportInformationAsString(transfers.get(transfer).getFinishingAirport()));
-            date = new Timestamp(10000);
+            date = new Timestamp(transfer_time.get(transfer_time.size() - transfer - 1).getKey());
             addNewDateToRoute(date);
-            date = new Timestamp(10000000);
+            date = new Timestamp(transfer_time.get(transfer_time.size() - transfer - 1).getValue());
             addNewDateToRoute(date);
             result.add(Double.toString(Math.round((transfers.get(transfer).getPrice() * 100) / 100.0)));
         }

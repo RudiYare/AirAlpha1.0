12c8c4749810c9b78b6b925beaf270c0344cb451
Joggor <artem_zinatulin643@mail.ru>
2021-10-09 23:36:49 +0300

    FX5

---
+++ src/ARTEM/Flight.java
@@ -0,0 +1,14 @@
+package ARTEM;
+
+import java.util.ArrayList;
+
+public class Flight {
+    public String from;
+    public String where;
+    public String flight_time;
+    public String price;
+    public String time_from;
+    public String time_where;
+    public ArrayList<Flight> transfers;
+    public Flight() {}
+}
